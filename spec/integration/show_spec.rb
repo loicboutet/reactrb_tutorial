9d7c32bc087d6234a10bff818541ee4590ef0ad8
Mitch VanDuyn <mitch@catprint.com>
2016-01-25 17:01:14 -0500

    moved scopes from controller to model, and added tests

--- spec/integration/show_spec.rb
+++ spec/integration/show_spec.rb
@@ -4,9 +4,14 @@ describe 'the todo app', :js => true do
 
     it 'shows the active todos' do
       5.times { FactoryGirl.create(:todo) }
-      visit "/todos/active"
+      2.times { FactoryGirl.create(:todo, complete: true) }
+      visit "/todos?scope=active"
+      byebug
       Todo.active do |todo|
         page.should have_content(todo.title)
+      end
+      Todo.complete do |todo|
+        page.should_not have_content(todo.title)
       end
     end
 
