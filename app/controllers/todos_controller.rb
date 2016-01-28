01fdace20eafe692d57342eb5c468b7540e19f75
Mitch VanDuyn <mitch@catprint.com>
2016-01-28 14:17:34 -0500

    cleaned up UI

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -42,8 +42,8 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
-        format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
-        format.json { render :show, status: :created, location: @todo }
+        format.html { redirect_to todos_url, notice: 'Todo was successfully created.' }
+        format.json { render :index, status: :created, location: @todo }
       else
         format.html { render :new }
         format.json { render json: @todo.errors, status: :unprocessable_entity }
