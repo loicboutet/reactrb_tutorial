b0774ee02552808597ba6a782e3eb54dc7a5556d
Loïc Boutet <loic@boutet.com>
2016-01-31 21:12:42 +0100

    initial todo_item for chapter 4

--- app/views/components/todos/todo_item.rb
+++ app/views/components/todos/todo_item.rb
@@ -3,25 +3,44 @@ module Components
     class TodoItem < React::Component::Base
 
       required_param :todo, type: Todo
+      define_state editing: false
 
-      def test
+
+      after_update do
+        edit_element = Element[".edit"]
+        edit_element.focus
+        `#{edit_element}[0].setSelectionRange(edit_element.val().length, edit_element.val().length)`
       end
 
       def render
-        li(class: (params.todo.complete ? "completed" : "")) do
+        li(class: "#{params.todo.complete ? "completed" : ""} #{state.editing ? "editing" : ""}") do
           div(class: "view")do
-            input(type: :checkbox, (params.todo.complete ? :defaultChecked : :unchecked) => true, :class => "toggle").on(:change) do
-              # params.todo.complete = !params.todo.complete
-              # puts params.todo.complete
-              # puts params.todo.save
-              t = Todo.find(params.todo.id)
-              t.complete = !t.complete
-              t.save
+            input(type: :checkbox, (params.todo.complete ? :checked : :unchecked) => true, :class => "toggle").on(:click) do
+              params.todo.complete = !params.todo.complete
+              params.todo.save
             end
             label do
               params.todo.title
+            end.on(:doubleClick) do
+              puts "in double click"
+              state.editing! true
+            end
+            a(class: :destroy).on(:click) do
+              params.todo.destroy
+            end
+
+          end
+          input(class: "edit", value: params.todo.title).form_control().on(:blur) do
+            state.editing! false if state.editing
+          end.on(:change) do |e|
+            params.todo.title = e.target.value
+          end.on(:key_down) do |e|
+            if e.key_code == 13
+              params.todo.save
+              state.editing! false
             end
           end
+
         end
       end
     end
